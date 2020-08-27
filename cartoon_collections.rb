c7ae4bc282a31a8d80bb3a8d3c1cacbeefd899a5
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-27 05:33:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,23 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+   array.each_with_index { |word, index| puts "#{index+1} #{word}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+   array.collect { |word| word.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+   array.any? { |word| word.length > 4}
 end
 
- def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find { |a| cheese_types.include?(a) }
-end
-
-    it "returns nil if the array does not contain a type of cheese" do
-      no_cheese = ["ham", "cellphone", "computer"]
-      expect(find_the_cheese(no_cheese)).to eq nil
-    end
-  end
+def find_the_cheese(array)
+   cheese_types = ["cheddar", "gouda", "camembert"]
+   array.find { |cheese| cheese_types.include?(cheese) }
 end
