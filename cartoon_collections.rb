3ad1fab59d860eee8cbd1945e784961613378384
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-27 05:31:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,14 +10,10 @@ def long_planeteer_calls# code an argument here
   # Your code here
 end
 
- describe "#find_the_cheese" do
-    it "returns the first element of the array that is cheese" do
-      contains_cheddar = ["banana", "cheddar", "sock"]
-      expect(find_the_cheese(contains_cheddar)).to eq 'cheddar'
-
-      contains_gouda = ["potato", "gouda", "camembert"]
-      expect(find_the_cheese(contains_gouda)).to eq 'gouda'
-    end
+ def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |a| cheese_types.include?(a) }
+end
 
     it "returns nil if the array does not contain a type of cheese" do
       no_cheese = ["ham", "cellphone", "computer"]
